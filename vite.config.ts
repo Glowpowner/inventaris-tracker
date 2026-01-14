fbcb23a051177800b4a12fd68a3cb8a03962021c
Glowpowner <milanhoogvliet@hotmail.nl>
2026-01-14 13:02:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/inventaris-tracker/",   // 👈 DIT TOEVOEGEN
+  base: "/inventaris-tracker/",   // 👈 DIT TOEVOEGEN oke
 
   server: {
     host: "::",
